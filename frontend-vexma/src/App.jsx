21d0ebd48320db75ed7d00910412ed62b83bca44
Agustina Cibin <agustinacibin@gmail.com>
2026-01-16 00:30:43 -0300

    Creacion de CSS principal para VehiculoList y modificaciones adicionales iniciales en App.css/App.jsx e index.css/index.html

--- frontend-vexma/src/App.jsx
+++ frontend-vexma/src/App.jsx
@@ -1,31 +1,93 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import VehiculoList from './pages/VehiculoList'
-import VehiculoForm from './pages/VehiculoForm'
-import TitularForm from './pages/TitularForm.jsx'
-import VehiculoDetalle from './pages/VehiculoDetalle.jsx'
-import VehiculoDocumentacion from './pages/VehiculoDocumentacion.jsx'
-import TitularList from './pages/TitularList.jsx'
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { FaCar, FaUsers, FaPlus, FaFileArchive } from 'react-icons/fa';
+import { useState } from "react";
+import "./App.css";
+import VehiculoList from "./pages/VehiculoList";
+import VehiculoForm from "./pages/VehiculoForm";
+import TitularForm from "./pages/TitularForm.jsx";
+import VehiculoDetalle from "./pages/VehiculoDetalle.jsx";
+import VehiculoDocumentacion from "./pages/VehiculoDocumentacion.jsx";
+import TitularList from "./pages/TitularList.jsx";
 
 function App() {
 
+  const [agregarNuevo, setAgregarNuevo] = useState(true)
+  const [mostrarBoton, setMostrarBoton] = useState(true)
+
   return (
     <BrowserRouter>
-      <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-        <h1>Gestión Vexma</h1>
+      <div className="header-class">
+        <header>VEXMA Management</header>
+      </div>
+
+      <div className="app-layout">
+        <div className="sidebar-class">
+          <nav>
+            <ul>
+              <li>
+                <span style={{marginLeft:"15px", marginBottom:"0px", fontSize:"28px", fontWeight:"500"}}>Menú</span>
+                <NavLink to="/"
+                  className={({ isActive }) => (isActive ? "active-link" : "")}
+                  onClick={() => {setAgregarNuevo(true), setMostrarBoton(true)}}>
+                  <FaCar /> Vehículos
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/titulares"
+                  className={({ isActive }) => (isActive ? "active-link" : "")}
+                  onClick={() => {setAgregarNuevo(false), setMostrarBoton(true)}}>
+                  <FaUsers /> Titulares
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/crear"
+                  className={({ isActive }) => (isActive ? "active-link" : "")}
+                  onClick={() => setMostrarBoton(false)}>
+                  <FaFileArchive /> Documentación
+                </NavLink>
+              </li>
+
+              {mostrarBoton && (
+                <li id="agregar-contenido-item">
+                  {agregarNuevo ? (
+                    <NavLink to="/crear"
+                      className={({ isActive }) => (isActive ? "active-link" : "")}>
+                        <FaPlus /> Nuevo Vehículo
+                    </NavLink>
+                  ) : (
+                    <NavLink to="/crear-titular"
+                      className={({ isActive }) => (isActive ? "active-link" : "")}>
+                        <FaPlus /> Nuevo Titular
+                    </NavLink>
+                  )}
+                  
+                </li>
+              )}
+            </ul>
+          </nav>
+        </div>
+
+        <div
+          className="main-content-container"
+          style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}
+        >
           <Routes>
-            <Route path='/' element={<VehiculoList/>}/>
-            <Route path='/crear' element={<VehiculoForm/>}/>
-            <Route path='/vehiculos/:id' element={<VehiculoDetalle/>}/>
-            <Route path='/vehiculos/:id/editar' element={<VehiculoForm/>}/>
-            <Route path='/vehiculos/:id/documentacion' element={<VehiculoDocumentacion/>}/>
-            <Route path='/titulares' element={<TitularList/>}/>
-            <Route path='/crear-titular' element={<TitularForm/>}/>
-            <Route path='/titulares/:id/editar' element={<TitularForm/>}/>
+            <Route path="/" element={<VehiculoList />} />
+            <Route path="/crear" element={<VehiculoForm />} />
+            <Route path="/vehiculos/:id" element={<VehiculoDetalle />} />
+            <Route path="/vehiculos/:id/editar" element={<VehiculoForm />} />
+            <Route
+              path="/vehiculos/:id/documentacion"
+              element={<VehiculoDocumentacion />}
+            />
+            <Route path="/titulares" element={<TitularList />} />
+            <Route path="/crear-titular" element={<TitularForm />} />
+            <Route path="/titulares/:id/editar" element={<TitularForm />} />
           </Routes>
+        </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend-vexma/src/pages/VehiculoList.jsx
+++ frontend-vexma/src/pages/VehiculoList.jsx
@@ -1,132 +1,182 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import VehiculoService from "../services/VehiculoService";
-import {FaEdit, FaEye, FaTrash, FaMoneyBillWave, FaUndo, FaFileArchive} from "react-icons/fa";
+import {
+  FaEdit,
+  FaEye,
+  FaTrash,
+  FaMoneyBillWave,
+  FaUndo,
+  FaFileArchive,
+  FaCar,
+  FaUsers,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
+import "../css/VehiculoList_css.css";
 
-function VehiculoList(){
-    const [vehiculos, setVehiculos] = useState([]);
+function VehiculoList() {
+  const [vehiculos, setVehiculos] = useState([]);
 
-    // Pide toda la lista de vehiculos
-    const cargarVehiculos = () => {
-        VehiculoService.obtenerTodos()
-                        .then(respuesta => {
-                            setVehiculos(respuesta.data);
-                            console.log("Datos recibidos: ", respuesta.data);
-                        })
-                        .catch(error => {
-                            console.error("Error al traer los vehiculos: ", error)
-                        });
-    };
+  // Pide toda la lista de vehiculos
+  const cargarVehiculos = () => {
+    VehiculoService.obtenerTodos()
+      .then((respuesta) => {
+        setVehiculos(respuesta.data);
+        console.log("Datos recibidos: ", respuesta.data);
+      })
+      .catch((error) => {
+        console.error("Error al traer los vehiculos: ", error);
+      });
+  };
 
-    useEffect(() => {
-        cargarVehiculos()
-    }, [])
+  useEffect(() => {
+    cargarVehiculos();
+  }, []);
 
-
-    // Para borrar algun vehiculo
-    const borrarVehiculo = (id) => {
-        if(window.confirm("¿Seguro que desea borrar permanentemente el vehiculo?")){
-            VehiculoService.borrar(id)
-                            .then(() => {
-                                cargarVehiculos()
-                            })
-                            .catch(error => console.error(error))
-        }
+  // Para borrar algun vehiculo
+  const borrarVehiculo = (id) => {
+    if (
+      window.confirm("¿Seguro que desea borrar permanentemente el vehiculo?")
+    ) {
+      VehiculoService.borrar(id)
+        .then(() => {
+          cargarVehiculos();
+        })
+        .catch((error) => console.error(error));
     }
+  };
 
+  const vehiculosActuales = vehiculos.filter((v) => !v.fechaEgreso);
+  const vehiculosVendidos = vehiculos.filter((v) => v.fechaEgreso);
 
-
-    // vista
-    return(
-        <div className="container">
-            <h2>Listado de Vehículos</h2>
-
-            <Link to='/crear'>
-                <button style={{marginBottom:"10px"}}>Nuevo vehículo</button>    
-            </Link>
-
-            <Link to='/titulares'>
-                <button style={{marginBottom:"10px"}}>Ver titulares</button>    
-            </Link>
-        
-            <table border="1" style={{ width: '100%', borderCollapse: 'collapse' }}>
-                <thead>
-                    <tr>
-                        <th>Patente</th>
-                        <th>Marca | Modelo</th>
-                        <th>Año</th>
-                        <th>Version</th>
-                        <th>Tipo</th>
-                        <th>Titular</th>
-                        <th>Estado</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {vehiculos.map(vehiculo => (
-                        <tr key={vehiculo.id} style={{ textAlign:"center"}}>
-                            <td>{vehiculo.patente}</td>
-                            <td>{vehiculo.marca} {vehiculo.modelo}</td>
-                            <td>{vehiculo.anio}</td>
-                            <td>{vehiculo.version}</td>
-                            <td>{vehiculo.tipo.replace(/_/g, ' ')}</td>
-                            <td>{vehiculo.titular ? `${vehiculo.titular.apellido}, ${vehiculo.titular.nombre}` : "-"}</td>
-
-                            <td>
-                                {vehiculo.fechaEgreso ? (
-                                    <span style={{ color: 'red', fontWeight: 'bold' }}>VENDIDO</span>
-                                ) : (
-                                    <span style={{ color: 'green', fontWeight: 'bold' }}>EN STOCK</span>
-                                )}
-                            </td>
-
-                            <td>
-                                {/*Ver detalles*/}
-                                <Link to={`/vehiculos/${vehiculo.id}`}>
-                                    <button style={{marginRight:"5px"}} title="Ver detalles">
-                                        <FaEye color='#5d8bffff'/>
-                                    </button>
-                                </Link>
-
-                                {/*Vender o reingresar (segun fechaEgreso)
-                                {
-                                    vehiculo.fechaEgreso == null ? (
-                                        <button style={{marginRight:"5px"}} title="Vender vehículo">
-                                            <FaMoneyBillWave color="green"/>
-                                        </button>
-                                    ) : (
-                                        <button style={{marginRight:"5px"}} title="Reingresar vehículo">
-                                            <FaUndo color="orange"/>
-                                        </button>
-                                    )
-                                }*/}
-
-                                {/* Ver Documentación Asociada */}
-                                <Link to={`/vehiculos/${vehiculo.id}/documentacion`}>
-                                    <button style={{marginRight:"5px"}} title="Ver documentación">
-                                        <FaFileArchive color='' FaFileArchive/>
-                                    </button>
-                                </Link>
-
-
-                                {/*Borrar*/}
-                                <button onClick={() => borrarVehiculo(vehiculo.id)} style={{marginRight:"5px"}} title="Borrar vehículo">
-                                    <FaTrash color="red"/>
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                    
-                </tbody>
-            </table>
-        
+  // vista
+  return (
+    <div className="vehiculo-list-container">
+      <div className="vehiculos-agencia">
+        <div>
+          <p className="vehiculos-agencia-numero">{vehiculos.length}</p>
+          <p className="vehiculos-agencia-texto">
+            {vehiculos.length > 1 || vehiculos.length == 0
+              ? "Vehículos Cargados"
+              : "Vehículo Cargado"}
+          </p>
         </div>
-    )
+        <div>
+          <p className="vehiculos-agencia-numero">{vehiculosActuales.length}</p>
+          <p className="vehiculos-agencia-texto">
+            {vehiculosActuales.length > 1 || vehiculosActuales.length == 0
+              ? "Vehículos en Agencia"
+              : "Vehículo en Agencia"}
+          </p>
+        </div>
+        <div>
+          <p className="vehiculos-agencia-numero">{vehiculosVendidos.length}</p>
+          <p className="vehiculos-agencia-texto">
+            {vehiculosVendidos.length > 1 || vehiculosVendidos.length == 0
+              ? "Vehículos Vendidos"
+              : " Vehículo Vendido "}
+          </p>
+        </div>
+      </div>
 
+      <div className="vehiculo-datos-lista-divisor"></div>
 
+      <h2>Listado de Vehículos</h2>
+
+      {vehiculos.map((vehiculo) => (
+        <div
+          key={vehiculo.id}
+          className="main-vehicle-container"
+          style={
+            vehiculo.fechaEgreso
+              ? { backgroundColor: "rgba(240, 248, 255, 0.41)" }
+              : { opacity: "100%" }
+          }
+        >
+          <div className="vehiculo-container">
+            <div
+              className="vehiculo-informacion-container"
+              style={
+                vehiculo.fechaEgreso ? { opacity: "65%" } : { opacity: "100%" }
+              }
+            >
+              <div id="main-car-info">
+                <span style={{ whiteSpace: "nowrap" }}>
+                  <strong>
+                    {vehiculo.marca} {vehiculo.modelo} {vehiculo.version}{" "}
+                    {vehiculo.anio}
+                  </strong>
+                </span>
+
+                <div
+                  style={{
+                    flex: 1,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      height: "30px",
+                      width: "1px",
+                      backgroundColor: "#99999970",
+                    }}
+                  ></div>
+                </div>
+
+                <span style={{ fontWeight: "bold", whiteSpace: "nowrap" }}>
+                  {vehiculo.patente}
+                </span>
+              </div>
+              <div id="car-owner">
+                <strong>Titular:</strong>{" "}
+                {vehiculo.titular
+                  ? vehiculo.titular.apellido + ", " + vehiculo.titular.nombre
+                  : "-"}
+              </div>
+            </div>
+
+            <div className="vehiculo-estado-container">
+              <p
+                style={
+                  vehiculo.fechaEgreso
+                    ? { color: "#ff4040" }
+                    : { color: "white" }
+                }
+              >
+                {vehiculo.fechaEgreso ? "VENDIDO" : "EN AGENCIA"}
+              </p>
+            </div>
+          </div>
+
+          <div className="acciones-container">
+            {/*Ver detalles*/}
+            <Link to={`/vehiculos/${vehiculo.id}`}>
+              <button style={{ marginRight: "5px" }} title="Ver detalles">
+                <FaEye color="rgb(117, 227, 255)" />
+              </button>
+            </Link>
+
+            {/* Ver Documentación Asociada */}
+            <Link to={`/vehiculos/${vehiculo.id}/documentacion`}>
+              <button style={{ marginRight: "5px" }} title="Ver documentación">
+                <FaFileArchive color="" FaFileArchive />
+              </button>
+            </Link>
+
+            {/*Borrar*/}
+            <button
+              onClick={() => borrarVehiculo(vehiculo.id)}
+              style={{ marginRight: "5px" }}
+              title="Borrar vehículo"
+            >
+              <FaTrash color="#fd4545" />
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-
 export default VehiculoList;
-
-
